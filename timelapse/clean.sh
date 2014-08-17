9d957b2f4b23e232a080c8787d4974be52d84196
Nathaniel Nelson <nelson.nleroy@gmail.com>
2014-08-17 13:53:50 -0600

    Cleaned up and improved the clean script

--- timelapse/clean.sh
+++ timelapse/clean.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-rm *.jpg
-rm *.avi
-rm *.txt
+FRAMES="*.jpg"
+FILES="files.txt"
+VIDEO="screenlapse.avi"
+
+if [ -e "$FRAMES" ]; then
+    rm "$FRAMES"
+fi
+
+if [ -e "$FILES" ]; then 
+    rm "$FILES"
+fi
+
+if [ -e "$VIDEO" ]; then
+    rm "$VIDEO5"
+fi
